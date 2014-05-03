2744c0ab9571501d1da394ac7dba75ae76fab7db
Anton Permiakov <anton.permiakov@gmail.com>
2014-05-03 09:28:40 +0300

    Undefined value fix

--- digitCapacity.js
+++ digitCapacity.js
@@ -9,12 +9,15 @@
         var selCell = this;
         if (selCell.children('span').length <= 0) {
             selCell.each(function() {
-                if ($(this).text().match(/[0-9]/)) {
-                    var numList = $(this).text().split(devider);
-                    $(this).html('<span style="text-align: right; display: inline-block">' + numList[0] + '</span><span style="text-align:left">' + devider + numList[1] + '</span>');
-                    if (spWidth < $(this).children('span:nth-child(1)').width()) {
-                        spWidth = $(this).children('span:nth-child(1)').width();
-                    }
+                var numList = $(this).text().split(devider);
+                if (numList[0] != undefined){
+                    $(this).html('<span style="text-align: right; display: inline-block">' + numList[0] + '</span>');
+                }
+                if (numList[1] != undefined){
+                    $(this).html('<span style="text-align: right; display: inline-block">' + numList[0] + '</span><span style="text-align:left">.' + numList[1] + '</span>');
+                }
+                if (spWidth < $(this).children('span:nth-child(1)').width()) {
+                    spWidth = $(this).children('span:nth-child(1)').width();
                 }
             });
             $(selCell).children("span:nth-child(1)").width(spWidth);
